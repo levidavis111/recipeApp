bee3b27386133784a75f6e2a630f46e12931eca8
levi davis <levidavis@pursuit.org>
2020-04-01 18:03:31 -0400

    Adds recipe info and constraints to detail vc

--- AzureOverall/View Controllers/AzureDetailViewController.swift
+++ AzureOverall/View Controllers/AzureDetailViewController.swift
@@ -23,6 +23,7 @@ class AzureDetailViewController: UIViewController {
         let label = UILabel()
         label.textColor = .black
         label.textAlignment = .center
+        label.numberOfLines = 0
         label.font = UIFont(name: "Georgia-Bold", size: 22)
         return label
     }()
@@ -31,6 +32,7 @@ class AzureDetailViewController: UIViewController {
         let label = UILabel()
         label.textColor = .black
         label.textAlignment = .center
+        label.numberOfLines = 0
         label.font = UIFont(name: "Georgia", size: 16)
         return label
     }()
@@ -44,16 +46,25 @@ class AzureDetailViewController: UIViewController {
         return stepper
     }()
 
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         addSubViews()
         constrainSubviews()
         setImage()
-        print(recipe!)
+        setText()
     }
     
     private func setImage() {
+        DispatchQueue.main.async {[weak self] in
+            self?.activityIndicator.startAnimating()
+        }
         if let currentRecipe = recipe {
             let urlString = "https://spoonacular.com/recipeImages/\(currentRecipe.id)-312x231.jpg"
             ImageManager.manager.getImage(urlStr: urlString) { [weak self](result) in
@@ -62,11 +73,27 @@ class AzureDetailViewController: UIViewController {
                     case .failure(let error):
                         print(error)
                         self?.imageView.image = UIImage(named: "noImage")
+                        self?.activityIndicator.stopAnimating()
                     case .success(let image):
                         self?.imageView.image = image
+                        self?.activityIndicator.stopAnimating()
                     }
                 }
             }
+        } else {
+            DispatchQueue.main.async {[weak self] in
+                self?.activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    private func setText() {
+        if let currentRecipe = recipe {
+            recipeTitleLabel.text = currentRecipe.title
+            recipeInfoLabel.text = "Holy Cow! This recipe makes the number \(currentRecipe.servings) total servings in only \(currentRecipe.readyInMinutes) minutes!!!"
+        } else {
+            recipeTitleLabel.text = "Uh oh!"
+            recipeInfoLabel.text = "There's been a mixup in the kitchen! Go back and search again! Sowwy!!!"
         }
     }
     
@@ -75,6 +102,7 @@ class AzureDetailViewController: UIViewController {
         view.addSubview(recipeTitleLabel)
         view.addSubview(recipeInfoLabel)
         view.addSubview(cartStepper)
+        view.addSubview(activityIndicator)
     }
     
     private func constrainSubviews() {
@@ -82,6 +110,7 @@ class AzureDetailViewController: UIViewController {
         constrainTitleLabel()
         constrainInfoLabel()
         constrainCartStepper()
+        constrainActivityIndicator()
     }
     
     private func constrainImageView() {
@@ -93,11 +122,31 @@ class AzureDetailViewController: UIViewController {
     }
     
     private func constrainTitleLabel() {
-        
+        recipeTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        [recipeTitleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+         recipeTitleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+         recipeTitleLabel.widthAnchor.constraint(equalToConstant: view.safeAreaLayoutGuide.layoutFrame.width / 1.25)].forEach {$0.isActive = true}
     }
     
-    private func constrainInfoLabel() {}
+    private func constrainInfoLabel() {
+        recipeInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        [recipeInfoLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+         recipeInfoLabel.topAnchor.constraint(equalTo: recipeTitleLabel.bottomAnchor, constant: 10),
+         recipeInfoLabel.bottomAnchor.constraint(lessThanOrEqualTo: cartStepper.topAnchor, constant: -10),
+         recipeInfoLabel.widthAnchor.constraint(equalToConstant: view.safeAreaLayoutGuide.layoutFrame.width / 1.25)].forEach {$0.isActive = true}
+    }
     
-    private func constrainCartStepper() {}
+    private func constrainCartStepper() {
+        cartStepper.translatesAutoresizingMaskIntoConstraints = false
+        [cartStepper.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+         cartStepper.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+            ].forEach {$0.isActive = true}
+    }
+    
+    private func constrainActivityIndicator(){
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        [activityIndicator.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+         activityIndicator.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)].forEach {$0.isActive = true}
+    }
 
 }
